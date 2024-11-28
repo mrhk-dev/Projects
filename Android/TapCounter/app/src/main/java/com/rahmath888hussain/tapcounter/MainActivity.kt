adca843e9bccb71df0952fcad45b9891138c1014
Rahmath-Lap <rahmath786khan@gmail.com>
2024-11-28 18:02:26 +0530

    added animation to the counter vlaue

--- Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
+++ Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.rahmath888hussain.tapcounter
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -10,17 +15,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-//    constants
+    //    constants
     private val COUNTER_VALUE_NAME = "counterValue"
 
-//    widgets declaration
+    //    widgets declaration
     private lateinit var prefs: SharedPrefs
-    private lateinit var addBt:Button
-    private lateinit var resetBtn:Button
-    private lateinit var counterTextView:TextView
+    private lateinit var addBt: Button
+    private lateinit var resetBtn: Button
+    private lateinit var counterTextView: TextView
 
-//    activity level variables
-    var counter_value:Int = 0
+    //    activity level variables
+    var counter_value: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,28 +38,69 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-       init()
+        init()
         counter_value = prefs.getInt(COUNTER_VALUE_NAME)
         counterTextView.text = counter_value.toString()
 
         addBt.setOnClickListener {
             counter_value = ++counter_value
             prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
-            counterTextView.text = counter_value.toString()
+//            counterTextView.text = counter_value.toString()
+
+            flipAnimation {
+                counterTextView.text = counter_value.toString()
+            }
+
         }
 
         resetBtn.setOnClickListener {
-            counter_value = 0
-            prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
-            counterTextView.text = counter_value.toString()
+//            animateCounter(counter_value, 0)
+//            counter_value = 0
+//            prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
+
+            flipAnimation {
+                counter_value = 0
+                prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
+                counterTextView.text = counter_value.toString()
+            }
+
         }
     }
 
-    fun init(){
+    fun init() {
         prefs = SharedPrefs.getInstance(this)
 
         addBt = findViewById(R.id.main_btn_increment)
         resetBtn = findViewById(R.id.main_btn_reset)
         counterTextView = findViewById(R.id.tv_counterValue)
     }
+
+    private fun flipAnimation(onAnimationEnd: () -> Unit) {
+        val animatorSet = AnimatorSet()
+        val flipX = ObjectAnimator.ofFloat(counterTextView, "rotationX", 0f, 90f)
+        flipX.duration = 100
+
+        val flipXBack = ObjectAnimator.ofFloat(counterTextView, "rotationX", 90f, 0f)
+        flipXBack.duration = 100
+
+        flipX.addUpdateListener { animation ->
+            val rotation = animation.animatedValue as Float
+            if (rotation >= 90f) {
+                val currentValue = counterTextView.text.toString().toIntOrNull() ?: 0
+                counterTextView.text = (currentValue + 1).toString()
+                flipX.removeUpdateListener { it == animation }
+            }
+        }
+
+        animatorSet.playSequentially(flipX, flipXBack)
+
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                onAnimationEnd()
+            }
+        })
+
+        animatorSet.start()
+    }
+
 }
