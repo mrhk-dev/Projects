ff90c16a72f14ecac6b7cf8f5023d4e0491f8652
Rahmath-Lap <rahmath786khan@gmail.com>
2024-11-28 15:53:54 +0530

    added Shared-Preferences and reset button with functionality

--- Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
+++ Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rahmath888hussain.tapcounter
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,22 +10,51 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+//    constants
+    private val COUNTER_VALUE_NAME = "counterValue"
 
+//    widgets declaration
+    private lateinit var prefs: SharedPrefs
+    private lateinit var addBt:Button
+    private lateinit var resetBtn:Button
+    private lateinit var counterTextView:TextView
 
+//    activity level variables
+    var counter_value:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val addBt:Button = findViewById(R.id.main_btn_add)
+
+       init()
+        counter_value = prefs.getInt(COUNTER_VALUE_NAME)
+        counterTextView.text = counter_value.toString()
 
         addBt.setOnClickListener {
-            Toast.makeText(this, "Add btn clicked", Toast.LENGTH_SHORT).show()
+            counter_value = ++counter_value
+            prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
+            counterTextView.text = counter_value.toString()
         }
+
+        resetBtn.setOnClickListener {
+            counter_value = 0
+            prefs.saveInt(COUNTER_VALUE_NAME, counter_value)
+            counterTextView.text = counter_value.toString()
+        }
+    }
+
+    fun init(){
+        prefs = SharedPrefs.getInstance(this)
+
+        addBt = findViewById(R.id.main_btn_increment)
+        resetBtn = findViewById(R.id.main_btn_reset)
+        counterTextView = findViewById(R.id.tv_counterValue)
     }
 }
