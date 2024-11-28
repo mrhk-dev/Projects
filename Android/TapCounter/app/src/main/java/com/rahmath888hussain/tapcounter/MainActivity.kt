8c1e4a5293bbef0abe290644704e6b270151f01e
Rahmath-Lap <rahmath786khan@gmail.com>
2024-11-28 20:07:55 +0530

    added option to change tile of the counter

--- Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
+++ Android/TapCounter/app/src/main/java/com/rahmath888hussain/tapcounter/MainActivity.kt
@@ -5,28 +5,42 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.transition.Visibility
 
 class MainActivity : AppCompatActivity() {
     //    constants
     private val counterValueName = "counterValue"
+    private val counterTitle = "Tap Counter"
 
     //    widgets declaration
-    private lateinit var prefs: SharedPrefs
     private lateinit var addBt: Button
     private lateinit var resetBtn: Button
     private lateinit var counterTextView: TextView
+    private lateinit var toolbar: Toolbar
+    private lateinit var titleTextView: TextView
+    private lateinit var editTextTitle: EditText
+private lateinit var setTitle : ImageView
+    private lateinit var menu: Menu
 
     //    activity level variables
     private var counterValue: Int = 0
+    private lateinit var prefs: SharedPrefs
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +53,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         init()
+
+        titleTextView.text = prefs.getString(counterTitle, defaultValue = "Tap Counter")
         counterValue = prefs.getInt(counterValueName)
         counterTextView.text = "$counterValue"
 
@@ -67,6 +86,18 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        setTitle.setOnClickListener{
+            if (editTextTitle.text.toString() != "" || editTextTitle.text.toString() != " ")
+                prefs.saveString(counterTitle, editTextTitle.text.toString())
+
+            editTextTitle.visibility = View.GONE
+            titleTextView.setText(editTextTitle.text.toString())
+            titleTextView.visibility = View.VISIBLE
+            setTitle.visibility = View.GONE
+            menu.findItem(R.id.editTitle).setVisible(true)
+        }
+
     }
 
     private fun init() {
@@ -75,6 +106,9 @@ class MainActivity : AppCompatActivity() {
         addBt = findViewById(R.id.main_btn_increment)
         resetBtn = findViewById(R.id.main_btn_reset)
         counterTextView = findViewById(R.id.tv_counterValue)
+        titleTextView = findViewById(R.id.tv_taskbarTitle)
+        editTextTitle = findViewById(R.id.et_taskbarTitle)
+        setTitle = findViewById(R.id.img_setTitle)
     }
 
     private fun flipAnimation(onAnimationEnd: () -> Unit) {
@@ -108,6 +142,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
+        if (menu != null) {
+            this.menu = menu
+        }
         return true
     }
 
@@ -118,6 +155,14 @@ class MainActivity : AppCompatActivity() {
 
                 val settingsBottomSheet = SettingsFragment()
                 settingsBottomSheet.show(supportFragmentManager, settingsBottomSheet.tag)
+                true
+            }
+
+            R.id.editTitle -> {
+                titleTextView.visibility = View.GONE
+                editTextTitle.visibility = View.VISIBLE
+                item.isVisible = false
+                setTitle.visibility = View.VISIBLE
                 true
             }
             else -> super.onOptionsItemSelected(item)
