8abfe967f1ccfb783894118b52a97c21e91e6850
rahmath <rahmath786khan@gmail.com>
2025-01-01 00:52:48 +0530

    Spring boot base code

---
+++ SpringBoot/spring-boot-project/src/main/java/com/rahmath/springboot/spring_boot_project/SpringBootProjectApplication.java
@@ -0,0 +1,11 @@
+package com.rahmath.springboot.spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+}
