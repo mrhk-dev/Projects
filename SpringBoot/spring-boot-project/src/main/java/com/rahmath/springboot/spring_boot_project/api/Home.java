13df74fcbe721c762921fb689bba1df0b7915de4
rahmath <rahmath786khan@gmail.com>
2025-01-01 22:36:10 +0530

    added hello message on body

---
+++ SpringBoot/spring-boot-project/src/main/java/com/rahmath/springboot/spring_boot_project/api/Home.java
@@ -0,0 +1,14 @@
+package com.rahmath.springboot.spring_boot_project.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping(value = "/")
+    public String sayHello() {
+        return "Hello Rahmath";
+    }
+
+}
