e705ca28f292d655cff3fd13846dfc3965b6f82f
rahmath <rahmath786khan@gmail.com>
2025-01-02 18:32:36 +0530

    added csrf token

--- SpringBoot/spring-boot-project/src/main/java/com/rahmath/springboot/spring_boot_project/api/EmployeeResource.java
+++ SpringBoot/spring-boot-project/src/main/java/com/rahmath/springboot/spring_boot_project/api/EmployeeResource.java
@@ -2,7 +2,9 @@ package com.rahmath.springboot.spring_boot_project.api;
 
 import com.rahmath.springboot.spring_boot_project.models.Employee;
 import com.rahmath.springboot.spring_boot_project.services.EmployeeService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,4 +41,17 @@ public class EmployeeResource {
     public void deleteEmp(@PathVariable(value = "empId") int id) {
         empService.deleteEmp(id);
     }
+
+
+    /*
+    * The post, put and delete method doesnt work if the csrf token is not provided or available.
+    * So we need to get the token and then provide the token to modift the data.
+    *
+    * in postman we use the tag called "X-CSRF-TOKEN" in attributes along with the auth data
+    * */
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest http){
+        return (CsrfToken) http.getAttribute("_csrf");
+    }
+
 }
